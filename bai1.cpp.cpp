a16e0135dcf88766e969f9d54288e562689eab06
TranBaBang16 <66945580+TranBaBang16@users.noreply.github.com>
2021-09-05 00:35:45 +0700

    Add files via upload

--- bai1.cpp.cpp
+++ bai1.cpp.cpp
@@ -3,7 +3,41 @@
 #include<stdio.h>
 #include<string>
 using namespace std;
-void Registe(string name, string pass)
+//==============
+long power(int x,int y)
+{
+	if (x==0)
+	{return 0;
+	}
+	long result=1;
+	for (int i=0;i<y;i++)
+	{
+		result=result*x;
+	}
+	return result;
+}
+
+//====================
+long string2int(string x)
+{
+	long result=0;
+	long n=x.length();
+	for (int i=0;i<n;i++)
+	{
+		if (x[i]<48&&x[i]>57)
+		{
+			return 0;
+		}
+		else
+		{
+		result=result+(x[i]-48)*power(10,n-1-i);	
+		}
+	}
+	return result;
+	
+}
+//====================
+void addUser(string name, string pass)
 {
 	
 string path="D:/Code/User/"+name;
@@ -21,6 +55,12 @@ else
 	cout<<"successfull!";
 }
 filedata.close();
+
+}
+//====================================
+
+void addList(string name)
+{
 string path_list="D:/Code/User/List";
 std::ofstream file_list(path_list.c_str(),std::ios::app);
 if(file_list.fail())
@@ -37,6 +77,9 @@ else
 }
 file_list.close();
 }
+
+//====================================
+
 bool FindUser(string name)
 {
 	ifstream filedata;
@@ -70,8 +113,40 @@ bool FindUser(string name)
 	
 	
 }
+//=================
+void TotalUser(int x)
+{
+	ofstream count;
+	string path="D:/Code/User/count";
+	count.open(path.c_str(),std::ios::trunc);
+	if (count.fail())
+	{
+		cout<<"can't file"<<endl;
+	}
+	else
+	{
+		count<<x;
+		cout<<"OK";
+	}
+	count.close();
+	
+}
+//==============
+int getCount()
+{
+	string path="D:/Code/User/count";
+	ifstream fileCount;
+	fileCount.open(path.c_str());
+	string data;
+	getline(fileCount,data);
+	int x=string2int(data);
+	return x;
+}
+
+//============
 int main()
 {
-
+int a=getCount();
+cout<<a+10;
 	return 0;
 }
